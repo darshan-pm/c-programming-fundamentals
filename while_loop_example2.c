ef95c578abb7d0a40e1d3c7bb1164f0ea6e7ccd4
Darshan <darshan200713@gmail.com>
2026-03-29 23:54:12 +0530

    added while loop example 2

---
+++ while_loop_example2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdio.h>
+int main() {
+    int number;
+
+    printf("Enter a number:");
+    scanf("%d", &number);
+
+    int count = 1;
+
+    while(count <= 10){
+        int product = number * count;
+        printf("%d*%d = %d\n", number, count, product);
+        count = count + 1;  
+    }
+    return 0;
+}
