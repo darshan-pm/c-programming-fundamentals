d3a987f98c1afe7ca2dfb233bdbbf04e976a406f
Darshan <darshan200713@gmail.com>
2026-04-01 10:20:12 +0530

    added array sum example1 program

---
+++ array_sum_example1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    printf("Enter the length of the array: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the %d elements of the array: ",n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int sum = 0;
+    for (int i = 0; i < n; i++) 
+        {
+        sum += arr[i];
+    }
+    printf("The sum of the array is: %d", sum);
+    return 0;
+}
